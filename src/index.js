35c185e82a64496b5bb07ee530988f95ece73a63
Matheus Wichman <matheushw@outlook.com>
2019-11-17 00:09:19 -0300

    Add files

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useDataProvider, useNotify, useRefresh } from 'react-admin';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+
+const OrderButtons = ({ record, resource, source }) => {
+  const dataProvider = useDataProvider();
+  const notify = useNotify();
+  const refresh = useRefresh();
+  const { order, sort, filter } = useSelector(
+    state => state.admin.resources[resource].list.params
+  );
+
+  const move = direction => {
+    dataProvider
+      .move(resource, { id: record.id, direction, filter })
+      .then(() => refresh())
+      .catch(error => notify(error.message, 'warning'));
+  };
+
+  const handleUp = e => {
+    e.stopPropagation();
+    move('up');
+  };
+
+  const handleDown = e => {
+    e.stopPropagation();
+    move('down');
+  };
+
+  return (
+    <ButtonGroup
+      size="small"
+      disabled={sort !== source || order !== 'ASC' || !record.ordering}
+    >
+      <Button>
+        <KeyboardArrowUpIcon onClick={handleUp} />
+      </Button>
+      <Button>
+        <KeyboardArrowDownIcon onClick={handleDown} />
+      </Button>
+    </ButtonGroup>
+  );
+};
+
+OrderButtons.propTypes = {
+  record: PropTypes.object,
+  resource: PropTypes.string,
+  source: PropTypes.string,
+};
+
+export default OrderButtons;
